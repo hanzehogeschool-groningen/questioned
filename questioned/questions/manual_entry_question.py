139a75afe2e211a21eee78d6c7e0a502fd398c65
David Visscher <david@davidvisscher.nl>
2020-05-13 13:52:48 +0200

    Enable the use of images for manual questions

--- questioned/questions/manual_entry_question.py
+++ questioned/questions/manual_entry_question.py
@@ -2,7 +2,9 @@
 This module defines the manual entry question.
 """
 
+import base64
 import random
+import logging
 
 from .question import Question
 
@@ -17,13 +19,24 @@ class ManualEntryQuestion(Question):
         """
         Generates an amount of manually input questions.
         """
+        # Pylint gets this wrong:
+        # pylint: disable=unsubscriptable-object
+
         out = []
         selection = list(random.sample(exam_spec['manual_entry_questions'], count))
         for selected_question in selection:
+            question_text = ""
+            if 'image' in selected_question.keys():
+                logging.debug('Encountered question with image path %s', selected_question['image'])
+                with open(selected_question['image'], 'rb') as image_file:
+                    image_base64 = base64.b64encode(image_file.read())
+                    question_text += f'<img src="data:image/png;base64, {image_base64.decode("utf-8")}" /><br/><br/>'
+
+            question_text += selected_question['question']
             out.append(
                 cls(
                     exam_spec,
-                    selected_question['question'],
+                    question_text,
                     selected_question['answer']
                 )
             )
