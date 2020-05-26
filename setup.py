aa6b861b1582aa273001dd7153f85a2c0e6a4435
David Visscher <david@davidvisscher.nl>
2020-05-26 17:25:15 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Questioned",
-    version="0.2.1",
+    version="0.3.0",
     packages=find_packages(),
     include_package_data=True,
 
