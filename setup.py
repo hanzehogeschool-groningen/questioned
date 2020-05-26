270408d7383d856e3f3235ea1f8ab9c09a39baaf
David Visscher <david@davidvisscher.nl>
2020-05-26 17:54:14 +0200

    Fix version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Questioned",
-    version="0.3.0u1",
+    version="0.3.1",
     packages=find_packages(),
     include_package_data=True,
 
