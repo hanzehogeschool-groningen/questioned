7f488512ff890ae11884d6b05ff8c3c9011d8928
David Visscher <david@davidvisscher.nl>
2020-05-28 18:27:00 +0200

    Bump version for breaking change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Questioned",
-    version="0.3.1",
+    version="0.4.0",
     packages=find_packages(),
     include_package_data=True,
 
