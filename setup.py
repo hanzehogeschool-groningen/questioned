d427f6dd7aab840eb7a71f9cb0c3c55e92683a82
David Visscher <david@davidvisscher.nl>
2020-06-15 14:00:17 +0200

    v0.4.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Questioned",
-    version="0.4.2",
+    version="0.4.3",
     packages=find_packages(),
     include_package_data=True,
 
